fe1754714414e637df89c44bdf89459774cd0c10
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-10-07 10:03:59 -0300

    First backend commit

---
+++ backend/src/test/java/com/ios/cabana/CabanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ios.cabana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
