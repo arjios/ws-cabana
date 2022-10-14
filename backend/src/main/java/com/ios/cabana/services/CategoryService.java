4f19dc2f290305aac90f7aa278eb8096a52063ba
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-10-14 16:42:38 -0300

    Exception Handler

--- backend/src/main/java/com/ios/cabana/services/CategoryService.java
+++ backend/src/main/java/com/ios/cabana/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.ios.cabana.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ios.cabana.dto.CategoryDTO;
 import com.ios.cabana.entities.Category;
 import com.ios.cabana.repositories.CategoryRepository;
+import com.ios.cabana.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -17,11 +19,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	@Transactional(readOnly = true)
+	@Transactional(readOnly= true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
-	
+	@Transactional(readOnly= true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		return new CategoryDTO(obj.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada")));
+	}
+
 }
