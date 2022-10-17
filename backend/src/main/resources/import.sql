6022b6064ea5ac9721301e17fd8f3e1cebe6bcdb
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-10-17 16:53:18 -0300

    User and Role entities

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,18 @@
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority) VALUES ('ROLA_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ATTENDANT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_STAFF');
+
+
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Jose', 'Patrocinio', 'jp@gmail.com', '123456', 1);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Antonio', 'Castro Alves', 'aca@gmail.com', '123456', 2);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('João', 'Maria', 'jm@gmail.com', '123456', 3);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Roberval', 'Taylor', 'rt@gmail.com', '123456', 1);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Manuel', 'Pereira', 'mp@gmail.com', '123456', 2);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Ana', 'Maria Siqueira', 'ams@gmail.com', '123456', 3);
+INSERT INTO tb_user (first_name, last_name, email, password, role_id) VALUES ('Pedro', 'Peixoto', 'pp@gmail.com', '123456', 3);
+
+
 INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Aves', NOW(), null);
 INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Carnes', NOW(), null);
 INSERT INTO tb_category (name, created_at, updated_at) VALUES ('Peixes', NOW(), null);
