0eb3ce4daa6cadb7bfdba5c35d34ccc9fc3cf8fc
Arjios <antonio.uab.ios.fisica@gmail.com>
2022-10-21 13:06:16 -0300

    ProductDTO

---
+++ backend/src/main/java/com/ios/cabana/dto/ProductDTO.java
@@ -0,0 +1,114 @@
+package com.ios.cabana.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.ios.cabana.entities.Category;
+import com.ios.cabana.entities.Product;
+
+public class ProductDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private Integer number;
+	private String name;
+	private String description;
+	private String urlImage;
+
+	private Instant date;
+	private Double price;
+
+	private CategoryDTO categoryDTO;
+
+	public ProductDTO() {
+
+	}
+
+	public ProductDTO(Long id, Integer number, String name, String description, String urlImage, Instant date,
+			Double price, Category category) {
+		this.id = id;
+		this.number = number;
+		this.name = name;
+		this.description = description;
+		this.urlImage = urlImage;
+		this.date = date;
+		this.price = price;
+		this.categoryDTO = new CategoryDTO(category);
+	}
+
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		number = entity.getNumber();
+		name = entity.getName();
+		description = entity.getDescription();
+		urlImage = entity.getUrlImage();
+		date = entity.getDate();
+		price = entity.getPrice();
+		categoryDTO = new CategoryDTO(entity.getCategory());
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrlImage() {
+		return urlImage;
+	}
+
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public CategoryDTO getCategoryDTO() {
+		return categoryDTO;
+	}
+
+	public void setCategoryDTO(CategoryDTO categoryDTO) {
+		this.categoryDTO = categoryDTO;
+	}
+
+}
